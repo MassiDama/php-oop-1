381adcbd1c1e99c5240b5f39b7f1dc1a7cdba164
Massidama <massidama@outlook.it>
2024-02-02 16:55:51 +0100

    feat: costruttore e motodo

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
 
-echo 'ciaoooo';
+    class Movie {
+
+    //  variabili d'istanza
+     public $title;
+     public $genres;
+     public $language;
+
+
+        // costruttore
+        public function __construct($title, $genres, $language) {
+
+        $this -> title = $title;
+        $this -> genres = $genres;
+        $this -> language = $language;
+
+        }
+
+        // metodo
+        public function getTitle () {
+            return $this -> title;
+        }
+
+    }
+
+    $movie1 = new Movie('Inglourious Basterds', 'War', 'English');
+    $movie2 = new Movie('La vita è bella', 'Comedy', 'Italian');
+
+
+    var_dump($movie1);
+
+    echo "<br>";
+
+    var_dump($movie2);
+
+    
+    
+   
+
+
+
+
+   
+
+    
